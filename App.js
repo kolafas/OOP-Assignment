ea5beaf8a56b2991c6f6534d03fd774ac42be055
kolafas <aladekolawole15@yahoo.com>
2016-08-17 12:33:22 +0100

    Functionality for searching the list for a related text

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 class NotesApplication {
 	let list = [];
-	/*
+	/**
 	* get the author
 	* get the list
 	 */
@@ -9,25 +9,51 @@ class NotesApplication {
 		this.list = noteList;
 	}
 
-	/*
-	* creating a function to take the note_cotent argument
+	/**
+	 * creating a function to take the note_cotent argument
 	 */
 	function create(note_content) {
 		this.list.push(note_content);
 		return this.list;
 	}
 
+	/**
+	 * function for listing out the notes in the list
+	 */
+
 	function listNotes() {
 
 		for(let i = 0; i < this.list.length; i++) {
-			return `Note ID ${i}
+			return `Note ID: ${i}
 			${this.list[i]}
 			
 			By Author ${author}`;
 		}
 	}
 
+	/**
+	 * function for get the note with the pass argument of note_id
+	 */
+
 	get(note_id) {
 		return this.list[note_id];
 	}
+
+	/** 
+	 * searching the list by index to find related test
+	 */
+
+	search(search_text) {
+		return "Showing results for " + search_text;
+
+		for(let i = 0; i<=this.list.length; i++) {
+			if(this.list[i].search(search_text)) {
+				return `Note ID: ${i}
+				${this.list[i]}
+
+				By Author ${author}`;
+			}
+		}
+	}
+	
 }
