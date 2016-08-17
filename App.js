0bbfb25758d4098a3371546e1f108ac6155cf48a
kolafas <aladekolawole15@yahoo.com>
2016-08-17 11:04:08 +0100

    functionality for note content

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 class NotesApplication {
-	let List = [];
+	let list = [];
+	/*
+	* get the author
+	* get the list
+	 */
 	constructor(author){
 		this.authors = author;
-		this.List = noteList;
+		this.list = noteList;
+	}
+
+	/*
+	* creating a function to take the note_cotent argument
+	 */
+	function create(note_content){
+		this.list.push(note_content);
 	}
 }
